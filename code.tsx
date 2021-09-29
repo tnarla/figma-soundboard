4324985ecfff12485c75cb7fedd2451e092292c1
Trushitha Narla <trushitha@squareup.com>
2021-09-28 17:07:28 -0700

    add property menu for edit

--- code.tsx
+++ code.tsx
@@ -7,6 +7,7 @@ const {
   Text: WidgetText,
   Frame,
   Image: WidgetImage,
+  usePropertyMenu,
 } = widget;
 
 interface Sound {
@@ -44,6 +45,22 @@ function Widget() {
 
   const [sounds, setSounds] = useSyncedState<Sound[]>("sounds", []);
 
+  usePropertyMenu(
+    [
+      {
+        tooltip: "Edit",
+        propertyName: "edit",
+        itemType: "action",
+        icon: '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5858 3.58579C14.3669 2.80474 15.6332 2.80474 16.4143 3.58579C17.1953 4.36683 17.1953 5.63316 16.4143 6.41421L15.6214 7.20711L12.793 4.37868L13.5858 3.58579Z" fill="#ffffff"/><path d="M11.3787 5.79289L3.00006 14.1716V17H5.82849L14.2072 8.62132L11.3787 5.79289Z" fill="#ffffff"/></svg>',
+      },
+    ],
+    async ({ propertyName }) => {
+      if (propertyName === "edit") {
+        await newSound({ url: "", name: "new" });
+      }
+    }
+  );
+
   function chunkArray(array, size) {
     let result = [];
     for (let i = 0; i < array.length; i += size) {
@@ -80,7 +97,7 @@ function Widget() {
       direction="vertical"
       horizontalAlignItems="center"
       verticalAlignItems="center"
-      width="hug-contents"
+      width={250}
       height="hug-contents"
       padding={16}
       fill="#FFFFFF"
